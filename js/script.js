5c07df2b88740517166d8863d3f0bf329c0bbccf
fmarabini93 <fmarabini93>
2021-04-27 15:31:15 +0200

    with-html

--- js/script.js
+++ js/script.js
@@ -5,9 +5,13 @@ var discount20 = cost - (cost / 5);
 var discount40 = cost - ((cost / 5) * 2);
 
 if (age < 18) {
-    var finalCost = discount20;
+    var finalCost = Math.round(discount40 * 100 + Number.EPSILON) / 100;
 } else if (age >= 65) {
-    var finalCost = discount40;
+    var finalCost = Math.round(discount40 * 100 + Number.EPSILON) / 100;
 } else {
-    var finalCost = cost;
+    var finalCost = Math.round(cost * 100 + Number.EPSILON) / 100;
 }
+
+document.getElementById("price").innerHTML = finalCost;
+document.getElementById("years").innerHTML = age;
+document.getElementById("kms").innerHTML = km;
